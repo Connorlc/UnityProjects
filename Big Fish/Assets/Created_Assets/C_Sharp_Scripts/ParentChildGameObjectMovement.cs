d0a88b23c413461fffc906cd986ada8d0db5d5a0
Connorlc <clchase@ucdavis.edu>
2016-07-13 10:37:44 -0700

    Up to date project folder

---
+++ Big Fish/Assets/Created_Assets/C_Sharp_Scripts/ParentChildGameObjectMovement.cs
@@ -0,0 +1,28 @@
+﻿// THIS SCRIPT HAS NOT BEEN TESTED!
+
+using UnityEngine;
+using System.Collections;
+
+
+// NOTE:	This script should be attached as a component to the parent object. It keeps a parent and child object's transforms in sync
+[ExecuteInEditMode]
+public class ParentChildGameObjectMovement : MonoBehaviour
+{
+	private Transform parentTrans;
+	private Transform childTrans;
+
+
+
+	private void Awake ()
+	{
+		parentTrans = GetComponent<Transform>();
+		childTrans = parentTrans.GetChild(2).transform;
+  } // Start()
+	
+
+
+	private void Update ()
+	{
+		parentTrans.position = new Vector2(childTrans.position.x, childTrans.position.y);
+	} // Update()
+} // class ParentChildGameObjectMovement
---
+++ Big Fish/Assets/UniformParentChildMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UniformParentChildObjectMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
