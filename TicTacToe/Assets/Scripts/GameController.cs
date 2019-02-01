575090d69c4d00af0754bd7dd807135ed2989207
Connorlc <clchase@ucdavis.edu>
2019-01-31 23:34:11 -0800

    finished section 8 of TicTacToe tutorial

--- TicTacToe/Assets/Scripts/GameController.cs
+++ TicTacToe/Assets/Scripts/GameController.cs
@@ -10,6 +10,7 @@ public class GameController : MonoBehaviour
     public Text gameOverText;
 
     private string playerSide;
+    private int moveCount;
 
     void SetGameControllerReferenceOnButtons()
     {
@@ -26,6 +27,8 @@ public class GameController : MonoBehaviour
 
     public void EndTurn()
     {
+        moveCount++;
+
         if(buttonList[0].text == playerSide && buttonList[1].text == playerSide && buttonList[2].text == playerSide) {
             GameOver();
         } else if(buttonList[3].text == playerSide && buttonList[4].text == playerSide && buttonList[5].text == playerSide) {
@@ -44,6 +47,11 @@ public class GameController : MonoBehaviour
             GameOver();
         }
 
+        if (moveCount >= 9)
+        {
+            SetGameOverText("It's a draw!");
+        }
+
         ChangeSides();
     }
 
@@ -54,13 +62,18 @@ public class GameController : MonoBehaviour
             buttonList[i].GetComponentInParent<Button>().interactable = false;
         }
 
-        gameOverText.text = playerSide + " Wins!";
-        gameOverPanel.SetActive(true);
+        SetGameOverText(playerSide + " Wins!");
     }
 
     void ChangeSides()
     {
         playerSide = (playerSide == "X") ? "O" : "X";
+    }
+
+    void SetGameOverText(string winner)
+    {
+        gameOverPanel.SetActive(true);
+        gameOverText.text = winner;
     }
 
     void Awake()
